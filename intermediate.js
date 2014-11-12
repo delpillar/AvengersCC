474afc7dd02948e7aebc10a7386d7ede44b608a2
delpillar <knivex27@gmail.com>
2014-11-12 15:05:54 -0500

    #7 - Create userReadInfo function to obtain user endpoint.

--- intermediate.js
+++ intermediate.js
@@ -2,33 +2,34 @@ var base = require('./base.js');
 
 exports.assemble = {
     addSchedule: 
-        function (parameters) {
-            var userEventEntry = {
+        function (parameters, response) {
+            var userEventEntry = { 
                 availability: JSON.stringify(parameters.availability),
                 Users_id: parameters.usersid, 
                 Events_id: parameters.eventsid
             };
             
-            base.dbInsert('userevents', userEventEntry);
-
-            return JSON.stringify({ 
-                msgType: 'updateSchedule', 
-                status: 'complete' 
-            });    
+            base.dbInsert('userevents', userEventEntry, response);
         },
+    
    viewUser:
-        function (parameters) {
-            var userReadInfo = { 
-                Users_id: parameters.usersid 
-            };
-
-            response = base.dbSelect('assemble' , 'users' , userReadInfo);
-
-            return JSON.stringify({
-                msgType: 'readuser',
-                data: response,
-                status: 'read'
-            });
-
+    function (parameters , response) {
+        var userReadInfo = { 
+            Users_id: parameters.usersid 
         };
 
+        var queryString = 'SELECT * FROM Users WHERE id = ' +  userReadInfo;
+        base.dbSelect(queryString, response)
+    },
+    addEvent:
+        function (parameters, response) {
+            var eventEntry = { 
+                                name: parameters.eventName,
+                                start_date: parameters.eventStartDate, 
+                                end_date: parameters.eventEndDate,
+                                description: parameters.eventDescription
+                            };
+
+            base.dbInsert('events', eventEntry, response);
+        }        
+} 
