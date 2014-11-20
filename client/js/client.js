089c7ce5e1a637668585c724b8789ae20fc5cd1b
Keane Quibilan <arv.r2vq@gmail.com>
2014-11-20 13:11:15 -0500

    Modify tabs to conform to style guides

--- client/js/client.js
+++ client/js/client.js
@@ -9,81 +9,88 @@ var oneDay = 86400;
 function runClient() {
 
     if (!Date.now) {
-        Date.now = function() { return new Date().getTime(); };
+        Date.now = function() {
+            return new Date().getTime();
+        };
     }
     var currentTime = Date.now();
     var tomorrow = currentTime + oneDay;
 
     var addScheduleParameters = {
-                                    msgType: 'addSchedule',
-                                    usersid: '1',
-                                    eventsid: '3',
-                                    availability: [ { start: currentTime, end: currentTime },
-                                                    { start: currentTime, end: currentTime } ]
-                            };
+        msgType: 'addSchedule',
+        usersid: '1',
+        eventsid: '3',
+        availability: [
+        { start: currentTime, end: currentTime }
+        ]
+    };
 
     var addEventParameters = {
-                                msgType: 'addEvent',
-                                eventName: 'meeting',
-                                eventStartDate: currentTime,
-                                eventEndDate: tomorrow,
-                                eventDescription: 'this meeting needs to be today'
-                            };
+        msgType: 'addEvent',
+        eventName: 'meeting',
+        eventStartDate: currentTime,
+        eventEndDate: tomorrow,
+        eventDescription: 'this meeting needs to be today'
+    };
 
     // Dummy hardcode adduser values.
     var addUserParameters = {
-                                msgType: 'addUser',
-                                userName: 'Charles Tapar',
-                                userPassword: 'makeitrain',
-                                userEmail: 'knivex27@gmail.com',
-                                availability: [ {start: currentTime, end: tomorrow},
-                                                {start: currentTime, end: tomorrow} ]
+        msgType: 'addUser',
+        userName: 'Charles Tapar',
+        userPassword: 'makeitrain',
+        userEmail: 'knivex27@gmail.com',
+        availability: [
+        {start: currentTime, end: tomorrow},
+        {start: currentTime, end: tomorrow}
+        ]
     };
 
 
     var updateScheduleParameters = {
-                                        msgType: 'updateSchedule',
-                                        usersid: '1',
-                                        eventsid: '1',
-                                        availability: [ { start: currentTime, end: tomorrow },
-                                                        { start: currentTime, end: tomorrow } ]
-                                    };
+        msgType: 'updateSchedule',
+        usersid: '1',
+        eventsid: '1',
+        availability: [
+        { start: currentTime, end: tomorrow },
+        { start: currentTime, end: tomorrow }
+        ]
+    };
 
     var updateUserParameters = {
-                                    msgType: 'updateUser',
-                                    username: 'kevp',
-                                    password: '123',
-                                    email: 'kevp@assemble.com',
-                                    defaultAvailability: addScheduleParameters.availability,
-                                    token: 'BCE',
-                                    usersid: '1'
-                                };
+        msgType: 'updateUser',
+        username: 'kevp',
+        password: '123',
+        email: 'kevp@assemble.com',
+        defaultAvailability: addScheduleParameters.availability,
+        token: 'BCE',
+        usersid: '1'
+    };
 
     var updateEventParameters = {
-                                msgType: 'updateEvent',
-                                eventName: 'gathering',
-                                eventStartDate: currentTime,
-                                eventEndDate: tomorrow,
-                                eventDescription: 'this meeting needs to be tomorrow',
-                                eventsid: '2'
-                            };
+        msgType: 'updateEvent',
+        eventName: 'gathering',
+        eventStartDate: currentTime,
+        eventEndDate: tomorrow,
+        eventDescription: 'this meeting needs to be tomorrow',
+        eventsid: '2'
+    };
 
     var viewEventParameters = {
-                                msgType: 'viewEvent',
-                                eventsid: '1'
-                            };
+        msgType: 'viewEvent',
+        eventsid: '1'
+    };
 
 
     var viewScheduleParameters = {
-                                    msgType: 'viewSchedule',
-                                    usersid: '1',
-                                    eventsid: '1'
-                                };
+        msgType: 'viewSchedule',
+        usersid: '1',
+        eventsid: '1'
+    };
 
     var viewUserParameters = {
-                                msgType: 'viewUser',
-                                usersid: '1'
-                            };
+        msgType: 'viewUser',
+        usersid: '1'
+    };
 
     //send(viewUserParameters);
     //send(updateScheduleParameters);
